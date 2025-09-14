29f5aa59dc19e514456aeb3725d798721b7c5965
ayhmdalila <ayhmdalilabzns@gmail.com>
2025-09-14 20:06:07 +0300

    added saved output

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ def main():
 
     video_path = "muhammad_ali.mkv"  # Change this to your video file path
     cap = cv2.VideoCapture(video_path)
-    
+
+    # Get video properties
+    width, height = 480, 250
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
+
     while cap.isOpened():
         ret, frame = cap.read()
-        frame = cv2.resize(frame, (480, 250 ))
         if not ret:
             break
+
+        frame = cv2.resize(frame, (width, height))
 
         # Convert the image to RGB
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -30,11 +37,13 @@ def main():
                     connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style()
                 )
 
+        out.write(frame)  # Save the frame to output video
         cv2.imshow("Face Keypoint Detection", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
+    out.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
